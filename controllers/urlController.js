1fc959125cb143686fcb814ec0b02e723e791d24
8309h <harshalwagh201718@gmail.com>
2024-08-08 00:17:27 +0530

    url-short

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -14,7 +14,6 @@ async function createShortUrl(req, res) {
       let shortCode;
       let isShortCodeUnique = false;
 
-      // Generate a unique short code
       while (!isShortCodeUnique) {
         shortCode = generateShortUrl();
         const existingUrl = await URL.findOne({ shortUrl: shortCode });
@@ -28,10 +27,8 @@ async function createShortUrl(req, res) {
         shortUrl
       });
 
-      // Save the URL document
       await url.save();
 
-      // Return both the long URL and short URL in the response
       res.json({ longUrl: url.longUrl, shortUrl: url.shortUrl });
     }
   } catch (err) {
@@ -50,16 +47,13 @@ function generateShortUrl(length = 8) {
   return shortUrl;
 }
 
-// Controller to redirect to the original URL
 async function redirectToLongUrl(req, res) {
   const shortCode = req.params.shortUrl;
 
   try {
-    // Find the URL document by short code
     const url = await URL.findOne({ shortUrl: shortCode });
 
     if (url) {
-      // Redirect to the original URL
       res.redirect(url.longUrl);
     } else {
       res.status(404).send('URL not found');
@@ -80,10 +74,3 @@ module.exports = {
 };
 
 
-
-// http://localhost:8080/url/redirect?shortUrl=5iScaj  => check in google
-// https://join.slack.com/t/slack-seo1731/shared_invite/zt-2d1lgddk5-oru~2UsSq1wpDNxWY5Ba7g
-//clinet id => 6667232878499.6680119475681
-// clirnt secete = >dd7a91ef90821bf7ef1c784657a5801e
-// signning => 680702d82888b8a3d7afc34d64d5ba8a
-//haZFMBTsAdRkDIunF8OcL1P =>  verificationtoken
